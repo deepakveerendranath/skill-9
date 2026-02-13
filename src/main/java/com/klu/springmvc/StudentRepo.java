c6ff4d9c26d5f63d7959e744e3b153c6c93864af
PANDIRI DEEPAK VEERENDRANATH <royd67065@gmail.com>
2026-02-13 08:12:22 +0530

    Add LibraryController and ignore system files

--- src/main/java/com/klu/springmvc/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.klu.springmvc;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class StudentRepo {
-  List<Student> sl = new ArrayList<>();
-}
--- src/main/java/com/klu/springmvc/StudentRepo1.java
+++
@@ -1,8 +0,0 @@
-package com.klu.springmvc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo1  extends JpaRepository<Student, Integer>{  
-	   
-        
-}
